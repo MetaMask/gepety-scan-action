21dc5949207147e48a8775a4644101a12bad6648
Nicholas Ellul <nicholas.ellul1@gmail.com>
2023-12-20 12:32:13 -0500

    Set up action to work with entrypoint script

--- src/entrypoint.ts
+++ src/entrypoint.ts
@@ -1,2 +1,19 @@
-const randomNumber: Number = Math.random();
-console.log('Hello World! ', randomNumber);
+import { context as githubContext } from "@actions/github";
+
+interface EntrypointArgs {
+  github: any;
+  context: typeof githubContext;
+}
+
+async function entrypoint({ github, context }: EntrypointArgs) {
+    console.log("Here are the objects you can use to interact with GitHub:");
+    console.log(JSON.stringify(github, null, 2));
+    console.log("Here's the context that triggered the workflow:");
+    console.log(JSON.stringify(githubContext, null, 2));
+    console.log("Here's the context passed in:");
+    console.log(JSON.stringify(context, null, 2));
+    console.log("Environment variables:");
+    console.log(JSON.stringify(process.env, null, 2));
+}
+
+module.exports = entrypoint;
